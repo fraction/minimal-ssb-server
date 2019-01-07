af61781fa403776d2214bbe59adacb356717e936
Christian Bundy <christianbundy@fraction.io>
2019-01-07 11:00:52 -0800

    Remove config merging

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 const Config = require('ssb-config/inject')
 const ssbKeys = require('ssb-keys')
 const path = require('path')
-const merge = require('lodash/merge')
 const fs = require('fs')
 
 const appName = process.env.ssb_appname || 'ssb'
 const opts = appName === 'ssb' ? null : null
 
-function Connections () {
-  const connections = (process.platform === 'win32')
-    ? undefined // this seems wrong?
-    : { incoming: { unix: [{ 'scope': 'local', 'transform': 'noauth' }] } }
-
-  return connections ? { connections } : {}
-}
-
-function Remote (config) {
-  const pubkey = config.keys.id.slice(1).replace(`.${config.keys.curve}`, '')
-  const remote = (process.platform === 'win32')
-    ? undefined // `net:127.0.0.1:${config.port}~shs:${pubkey}` // currently broken
-    : `unix:${path.join(config.path, 'socket')}:~noauth:${pubkey}`
-
-  return remote ? { remote } : {}
-}
-
 let config = Config(appName, opts)
 config.keys = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
-
-config = merge(
-  config,
-  Connections(),
-  Remote(config)
-)
 
 var createSbot = require('ssb-server')
   .use(require('ssb-server/plugins/master'))
